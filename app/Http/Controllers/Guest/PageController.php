7a008b2b787fab5b0ff9c75429e1250d8395e2fc
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-06-29 16:55:06 +0200

    add controllo creazione nuovo treno

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -12,18 +12,19 @@ class PageController extends Controller
 
     $trains = Trains::all();
 
-    $interegionale = new Trains();
-    $interegionale->azienda = 'Trenitalia';
-    $interegionale->stazione_di_partenza = 'Bari';
-    $interegionale->stazione_di_arrivo = 'Foggia';
-    $interegionale->orario_di_partenza = '10:00:00';
-    $interegionale->orario_di_arrivo = '10:30:00';
-    $interegionale->codice_treno = '83993662';
-    $interegionale->numero_carrozze = '5';
-    $interegionale->in_orario = '1';
-    $interegionale->cancellato = '0';
-    $interegionale->save();
-
+    if(!Trains::where("codice_treno", "83993662")->first()) {
+        $interegionale = new Trains();
+        $interegionale->azienda = 'Trenitalia';
+        $interegionale->stazione_di_partenza = 'Bari';
+        $interegionale->stazione_di_arrivo = 'Foggia';
+        $interegionale->orario_di_partenza = '10:00:00';
+        $interegionale->orario_di_arrivo = '10:30:00';
+        $interegionale->codice_treno = '83993662';
+        $interegionale->numero_carrozze = '5';
+        $interegionale->in_orario = '1';
+        $interegionale->cancellato = '0';
+        $interegionale->save();
+    }
 
     return view('welcome', compact('trains') );
 
